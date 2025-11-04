accf3c9836ecaff206b50550b237179061cfae21
Dom056 <dominicdawson99@gmail.com>
2025-11-04 17:53:24 +0000

    NavBar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import NavBar from "./components/navBar";
 
+export default function App() {
+  return (
+    <>
+    <div>
+      <h1>Welcome to the British Museum</h1>
+      <NavBar />
+    </div>
+    </>
+  );
+}
---
+++ src/components/navBar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+  return (
+    <nav>
+        <ul className="NavBar">
+      <button className="Home">Home</button>
+        <button className="About">About</button>
+        <button className="Map">Map</button>
+        <button className="SupportUs">Support Us</button>
+        <button className="Contact">Contact</button>
+        </ul>
+    </nav>
+  );
+}
+
